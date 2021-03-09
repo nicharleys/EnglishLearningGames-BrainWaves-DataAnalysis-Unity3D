595a713347d385434bfeb3941efaf589932ca11a
nicharleys <zxc5679120@gmail.com>
2021-03-10 04:02:55 +0800

    UI Update Vision 2

---
+++ BrainWave/Assets/Images/UIManagement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+public class UIManagement : MonoBehaviour {
+	[SerializeField] UIControll startUI;
+ 	[SerializeField] UIControll[] secondUI;
+	[SerializeField] UIControll[] thirdUI;
+	[SerializeField] UIControll[] fourthUI;
+	// Use this for initialization
+	[TextArea(5, 10)]
+	[SerializeField] string showUIStatus;
+	string temp;
+	void Update() {
+		UIStatus();
+	}
+	private void UIStatus() {
+		temp += "UI Page : " + startUI.nowUINumber + " UI Name : " + startUI.uiName + "\n";
+		UIData(secondUI);
+		UIData(thirdUI);
+		UIData(fourthUI);
+		showUIStatus = temp;
+		temp = "";
+
+	}
+	private void UIData(UIControll[] uiLevel) {
+		for (int i = 0; i < uiLevel.Length; i++) {
+			temp += "UI Page : " + uiLevel[i].nowUINumber + " UI Name : " + uiLevel[i].uiName + "\n";
+		}
+	}
+}
